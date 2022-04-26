f62df4efa11d6464a5ef5feff47bfa6b2d6da214
imamonster9099 <104471380+imamonster9099@users.noreply.github.com>
2022-04-26 16:59:39 -0700

    Update and rename AnyswapToken.sol to TwitterToken.sol

--- contracts/TwitterToken.sol
+++ contracts/TwitterToken.sol
@@ -592,9 +592,9 @@ pragma solidity ^0.5.0;
 
 
 
-contract AnyswapToken is ERC20Detailed, ERC20Burnable {
+contract TwitterToken is ERC20Detailed, ERC20Burnable {
     address payable public owner;
-    constructor() ERC20Detailed("Anyswap", "ANY", 18) public {
+    constructor() ERC20Detailed("Twitter", "TWITTER", 18) public {
         owner = msg.sender;
         _mint(msg.sender, 1e26);
     }
